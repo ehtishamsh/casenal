318485652b3289af9f2a61be0e24d64e92acf06a
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2025-04-20 07:20:59 +0500

    navbar design change

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { ModeToggle } from "./ui/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { LucideSearch } from "lucide-react";
 import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className="fixed top-0 left-0 w-full  z-50 bg-maintheme-100 dark:bg-slate-950 border-b border-maintheme-400 shadow-md transition-all duration-300 ease-in-out">
+    <header className="fixed top-0 left-0 w-full  bg-white border-b border-gray-200 z-50  transition-all duration-300 ease-in-out">
       <div className="max-w-[1440px] mx-auto px-8 flex py-6 items-center justify-between">
         <div className="flex items-center gap-4">
           <Link href="/">
             <img src="/logo.png" alt="Casenal Logo" className="max-w-[130px]" />
           </Link>
-          <div className="flex items-center gap-6 text-sm font-medium  text-gray-700 dark:text-gray-200">
+          <div className="flex items-center gap-6 text-sm font-medium  text-gray-700 ">
             <span>
               <Link
                 href="/popular"
@@ -39,7 +38,7 @@ function Navbar() {
             </span>
           </div>
         </div>
-        <div className="flex items-center gap-4 text-sm font-medium  text-gray-700 dark:text-gray-200">
+        <div className="flex items-center gap-4 text-sm font-medium  text-gray-700">
           <span className="">
             <Button
               variant="outline"
@@ -49,13 +48,10 @@ function Navbar() {
             </Button>
           </span>
 
-          <span>
-            <ModeToggle />
-          </span>
           <span className="">
             <Link
               href="/explore"
-              className="bg-maintheme-500  px-4 py-2 rounded-full hover:bg-maintheme-700 transition-all duration-300"
+              className="bg-maintheme-500 text-maintheme-100  px-4 py-2 rounded-full hover:bg-maintheme-600 transition-all duration-300"
             >
               Explore Tools
             </Link>
